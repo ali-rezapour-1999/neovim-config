b30cba4f5cb5e8f22c464652ae41180620ee1e48
alirg1999 <ali.programer.1999@gmail.com>
2025-01-28 13:27:55 +0330

    feat : my last config fork CRAFTDOGS nvim config

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,210 @@
+return {
+	{
+		enabled = false,
+		"folke/flash.nvim",
+		---@type Flash.Config
+		opts = {
+			search = {
+				forward = true,
+				multi_window = false,
+				wrap = false,
+				incremental = true,
+			},
+		},
+	},
+
+	{
+		"echasnovski/mini.hipatterns",
+		event = "BufReadPre",
+		opts = {
+			highlighters = {
+				hsl_color = {
+					pattern = "hsl%(%d+,? %d+%%?,? %d+%%?%)",
+					group = function(_, match)
+						local utils = require("solarized-osaka.hsl")
+						--- @type string, string, string
+						local nh, ns, nl = match:match("hsl%((%d+),? (%d+)%%?,? (%d+)%%?%)")
+						--- @type number?, number?, number?
+						local h, s, l = tonumber(nh), tonumber(ns), tonumber(nl)
+						--- @type string
+						local hex_color = utils.hslToHex(h, s, l)
+						return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
+					end,
+				},
+			},
+		},
+	},
+
+	{
+		"dinhhuy258/git.nvim",
+		event = "BufReadPre",
+		opts = {
+			keymaps = {
+				-- Open blame window
+				blame = "<Leader>gb",
+				-- Open file/folder in git repository
+				browse = "<Leader>go",
+			},
+		},
+	},
+
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+			},
+			"nvim-telescope/telescope-file-browser.nvim",
+		},
+		keys = {
+			{
+				"<C-m>",
+				function()
+					local word = vim.fn.expand("<cword>")
+					require("telescope.builtin").grep_string({
+						search = word,
+						only_sort_text = true,
+						path_display = { "shorten" },
+						cwd = vim.fn.getcwd(),
+					})
+				end,
+				desc = "Grep word under cursor",
+			},
+			{
+				";r",
+				function()
+					require("telescope.builtin").resume()
+				end,
+				desc = "Resume last Telescope picker",
+			},
+			{
+				";b",
+				function()
+					require("telescope.builtin").buffers()
+				end,
+				desc = "Search buffers",
+			},
+			{
+				"nn",
+				function()
+					local telescope = require("telescope")
+					local function telescope_buffer_dir()
+						return vim.fn.expand("%:p:h")
+					end
+
+					telescope.extensions.file_browser.file_browser({
+						path = "%:p:h",
+						cwd = telescope_buffer_dir(),
+						respect_gitignore = false,
+						hidden = true,
+						grouped = true,
+						previewer = false,
+						initial_mode = "normal",
+						layout_config = { height = 30, width = 110 },
+					})
+				end,
+				desc = "Open File Browser with the path of the current buffer",
+			},
+		},
+		config = function(_, opts)
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
+			local fb_actions = require("telescope").extensions.file_browser.actions
+
+			opts = opts or {}
+
+			opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+				wrap_results = true,
+				layout_strategy = "horizontal",
+				layout_config = { prompt_position = "top" },
+				sorting_strategy = "ascending",
+				winblend = 0,
+				mappings = {
+					n = {},
+				},
+			})
+
+			opts.pickers = opts.pickers
+				or {
+					diagnostics = {
+						theme = "ivy",
+						initial_mode = "normal",
+						layout_config = {
+							preview_cutoff = 9999,
+						},
+					},
+				}
+
+			opts.extensions = opts.extensions
+				or {
+					file_browser = {
+						theme = "dropdown",
+						hijack_netrw = true,
+						mappings = {
+							["n"] = {
+								["N"] = fb_actions.create,
+								["h"] = fb_actions.goto_parent_dir,
+								["/"] = function()
+									vim.cmd("startinsert")
+								end,
+								["<C-u>"] = function(prompt_bufnr)
+									for i = 1, 10 do
+										actions.move_selection_previous(prompt_bufnr)
+									end
+								end,
+								["<C-d>"] = function(prompt_bufnr)
+									for i = 1, 10 do
+										actions.move_selection_next(prompt_bufnr)
+									end
+								end,
+								["<PageUp>"] = actions.preview_scrolling_up,
+								["<PageDown>"] = actions.preview_scrolling_down,
+							},
+						},
+					},
+				}
+
+			-- Apply the configuration
+			telescope.setup(opts)
+			require("telescope").load_extension("fzf")
+			require("telescope").load_extension("file_browser")
+		end,
+	},
+
+	{
+		"saghen/blink.cmp",
+		opts = {
+			completion = {
+				menu = {
+					winblend = vim.o.pumblend,
+				},
+			},
+			signature = {
+				window = {
+					winblend = vim.o.pumblend,
+				},
+			},
+		},
+	},
+
+	{
+		"kdheepak/lazygit.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim" },
+		config = function() end,
+		keys = {
+			{ "gl", ":LazyGit<CR>", desc = "Open Lazygit" },
+		},
+		cmd = "LazyGit",
+	},
+	
+	{
+		"lewis6991/gitsigns.nvim",
+		lazy = false,
+		config = function()
+			require("gitsigns").setup({
+				current_line_blame = false,
+			})
+		end,
+	},
+}
